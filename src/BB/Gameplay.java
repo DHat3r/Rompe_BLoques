fc327470dc62cebb5564020745d81ee3cb4c9ba7
DHat3r <leandronorambuena1802@gmail.com>
2026-09-11 16:50:19 -0300

    arreglados los key events

--- src/BB/Gameplay.java
+++ src/BB/Gameplay.java
@@ -6,8 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import javax.swing.Timer;
 
+import javax.swing.Timer;
 import javax.swing.JPanel;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener{
@@ -60,6 +60,22 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if(playerX >= 600){
+                playerX = 600;
+            }else{
+                moveRight();
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            if(playerX < 10){
+                playerX = 10;
+            }else{
+                moveLeft();
+            }
+        }
+ 
+
         throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
     }
 
@@ -75,5 +91,14 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
         throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
 
+    private void moveRight(){
+        play = true;
+        playerX += 20;
+    }
+
+    private void moveLeft(){
+        play = true;
+        playerX -= 20;
+    }
 
 }
